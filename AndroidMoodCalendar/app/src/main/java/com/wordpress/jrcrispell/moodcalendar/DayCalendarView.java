eed69eb6e76b55262b95a343382db849014e9c94
Jason Crispell <jrcrispell@gmail.com>
2017-09-06 01:16:47 -0400

    Got scrollview to work but it's with a pretty ugly hard-coded bandaid solution

--- AndroidMoodCalendar/app/src/main/java/com/wordpress/jrcrispell/moodcalendar/DayCalendarView.java
+++ AndroidMoodCalendar/app/src/main/java/com/wordpress/jrcrispell/moodcalendar/DayCalendarView.java
@@ -174,13 +174,58 @@ public class DayCalendarView extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int desiredWidth = getSuggestedMinimumWidth() + getPaddingLeft() + getPaddingRight();
-        int desiredHeight = getSuggestedMinimumHeight() + getPaddingTop() + getPaddingBottom();
+//
+//        int suggestedWidth = getSuggestedMinimumWidth();
+//        int suggestedHeight = getSuggestedMinimumHeight();
+//
+//        int desiredWidth = suggestedWidth + getPaddingLeft() + getPaddingRight();
+//        int desiredHeight = suggestedHeight + getPaddingTop() + getPaddingBottom();
+//
+//        int widthResult = measureDimension(desiredWidth, widthMeasureSpec);
+//        int heightResult = measureDimension(desiredHeight, heightMeasureSpec);
+//
+//        //setMeasuredDimension(widthResult, heightResult);
+//
 
-        //setMeasuredDimension(measureDimension(desiredWidth, widthMeasureSpec),
-        //        measureDimension(desiredHeight, heightMeasureSpec));
+        //TODO - fix this bandaid
+          setMeasuredDimension(2000, 2900);
 
-        setMeasuredDimension(2000,
-                2000);
+
+        // take 2
+//        int desiredWidth = 100;
+//        int desiredHeight = 100;
+//
+//        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+//        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+//        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+//        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+//
+//        int width;
+//        int height;
+//
+//        //Measure Width
+//        if (widthMode == MeasureSpec.EXACTLY) {
+//            //Must be this size
+//            width = widthSize;
+//        } else if (widthMode == MeasureSpec.AT_MOST) {
+//            //Can't be bigger than...
+//            width = Math.min(desiredWidth, widthSize);
+//        } else {
+//            //Be whatever you want
+//            width = desiredWidth;
+//        }
+//
+//        //Measure Height
+//        if (heightMode == MeasureSpec.EXACTLY) {
+//            //Must be this size
+//            height = heightSize;
+//        } else if (heightMode == MeasureSpec.AT_MOST) {
+//            //Can't be bigger than...
+//            height = Math.min(desiredHeight, heightSize);
+//        } else {
+//            //Be whatever you want
+//            height = desiredHeight;
+//        }
+//        setMeasuredDimension(width, height);
     }
 }
