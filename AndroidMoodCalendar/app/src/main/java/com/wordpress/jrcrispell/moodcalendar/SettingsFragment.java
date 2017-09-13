18cf6a826b50ad503b0fe7a08b07d9910d36e443
Jason Crispell <jrcrispell@gmail.com>
2017-09-13 11:33:50 -0400

    Progress on settings fragment

--- AndroidMoodCalendar/app/src/main/java/com/wordpress/jrcrispell/moodcalendar/SettingsFragment.java
+++ AndroidMoodCalendar/app/src/main/java/com/wordpress/jrcrispell/moodcalendar/SettingsFragment.java
@@ -1,11 +1,15 @@
 package com.wordpress.jrcrispell.moodcalendar;
 
 import android.app.Fragment;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class SettingsFragment extends PreferenceFragment {
 
@@ -19,6 +23,21 @@ public class SettingsFragment extends PreferenceFragment {
 
         addPreferencesFromResource(R.xml.prefs_screen);
 
+        Preference startTime = findPreference("com.wordpress.jrcrispell.moodcalendar.notifications_start");
+        startTime.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                TimePickerDialog dialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                        Toast.makeText(getActivity(), i, Toast.LENGTH_SHORT).show();
+                    }
+                }, 8, 0, false);
+                dialog.show();
+                return true;
+            }
+        });
     }
+
 
 }
